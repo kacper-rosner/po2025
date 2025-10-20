68f4e2dc5a3439fb5814d8589f163dadb56478f5
Rosner <rosner@kacper.com>
2025-10-20 19:56:34 +0200

    Lab3

---
+++ Lab3/src/CodingBat.java
@@ -0,0 +1,44 @@
+public class CodingBat {
+    public static String firstHalf(String str) {
+        char[] a= str.toCharArray();
+        int l=str.length()/2;
+        char result[] =  new char [l];
+
+        for (int i=0; i<l; i++){
+            result[i]=a[i];
+        }
+        String resultstr = new String(result);
+        return resultstr;
+    }
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        if(!weekday || vacation){
+            return true;
+        }
+        else return false;
+    }
+    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if ((!aSmile && !bSmile ) || (aSmile && bSmile)){
+            return true;
+        }
+        else return false;
+    }
+
+    public static boolean firstLast6(int[] nums) {
+        if(nums.length>=1){
+            if (nums[0]==6){return true;}
+            else if (nums[nums.length-1]==6){return true;}
+            else return false;
+        }
+        else return false;
+    }
+    public static void main(String[] args){
+        System.out.println(firstLast6(new int[]{1,2,3,4,5,6,7,8,9,10}));
+        System.out.println(firstHalf("ABCDSEasdf"));
+        System.out.println(monkeyTrouble(true,true));
+        System.out.println(sleepIn(true,true));
+
+
+
+    }
+
+}
---
+++ Lab3/src/CodingBatTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Test.*;
+
+public class CodingBatTest{
+    @Test
+    public void firstHalf(){
+        assertEquals()
+    }
+    @Test
+    public void monkeyTrouble(){
+
+    }
+    @Test
+    public void sleepIn(){
+
+    }
+    @Test
+    public void firstLast6(){
+        assertEquals(true,[1,2,6]);
+        fir
+    }
+
+}
